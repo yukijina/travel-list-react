bf1d92e461522e24d7f427728cc8428d015bf435
yuki <38411009+yukijina@users.noreply.github.com>
2025-12-08 06:29:47 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './Logo';
+import Form from './Form';
+import Stats from './Stats';
+import PackingList from './PackingList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>Hello</p>
+      <Logo />
+      <Form />
+      <Stats />
+      <PackingList />
     </div>
   );
 }
