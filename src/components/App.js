43d8eb23d1a71ed28cc5417f06ff7e841221ea90
yuki <38411009+yukijina@users.noreply.github.com>
2025-12-09 06:38:50 -0800

    added initialdata (temporaly)

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,28 @@ import Form from './Form';
 import Stats from './Stats';
 import PackingList from './PackingList';
 
+const initialItems = [
+  {
+    id: 1,
+    description: 'Passports',
+    quantity: 2,
+    packed: false,
+  },
+  {
+    id: 2,
+    description: 'Socks',
+    quantity: 12,
+    packed: false,
+  },
+];
+
 function App() {
   return (
     <div className='App'>
       <Logo />
       <Form />
-      <Stats />
       <PackingList />
+      <Stat />
     </div>
   );
 }
