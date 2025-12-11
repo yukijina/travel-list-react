43258b6eb7a1f4f26fecfa6d65c2f170131113dd
yuki <38411009+yukijina@users.noreply.github.com>
2025-12-11 06:43:39 -0800

    created form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,8 +3,17 @@ import '../index.css';
 
 export default function Form() {
   return (
-    <div className='add-form'>
+    <form className='add-form'>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select name='' id=''>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type='text' placeholder='Item...' />
+      <button>Add</button>
+    </form>
   );
 }
