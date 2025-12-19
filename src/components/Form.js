62d62e08b3a67cbe876bd7dad7052a1a630ee536
yuki <38411009+yukijina@users.noreply.github.com>
2025-12-19 06:40:23 -0800

    created function to handle the input item and format item name

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,55 @@
 import React from 'react';
+import { useState } from 'react';
 import '../index.css';
 
-export default function Form() {
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  const formattedDescription = (string) => {
+    const trimmed = string.trim();
+    const formatted = trimmed.charAt(0).toUpperCase().concat(trimmed.slice(1));
+    return formatted;
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // nothing happen if description was empty
+    if (!description) return;
+
+    const newItem = {
+      description: formattedDescription(description),
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+
+    // crate a new object
+    onAddItems(newItem);
+    setDescription('');
+    setQuantity(1);
+  }
+
   return (
-    <form className='add-form'>
+    // onSbmit in <form>, not onClick <button>. If onClick is added to the button instead, it
+    // won't trigger when a user pressed enter for submit
+
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select name='' id=''>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type='text' placeholder='Item...' />
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
