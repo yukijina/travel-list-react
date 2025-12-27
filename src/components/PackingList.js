1d85b0fbbf4332b422e9adc6db340c34e80d326c
yuki <38411009+yukijina@users.noreply.github.com>
2025-12-26 16:02:47 -0800

    added select options. create a function for the selection and sort items

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../index.css';
 import Item from './Item';
 
 export default function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState('input');
+
+  let sortedItems;
+
+  if (sortBy === 'input') sortedItems = items;
+  if (sortBy === 'description') {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+  if (sortBy === 'packed') {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className='list'>
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -15,6 +31,14 @@ export default function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className='actions'>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value='input'>sort by input order</option>
+          <option value='description'>sort by description</option>
+          <option value='packed'>sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
